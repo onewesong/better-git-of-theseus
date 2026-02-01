9a7730874d68ae4a8ad1d587d542d69f3bd56a33
wesong <onewesong@gmail.com>
2026-02-01 16:48:43 +0800

    fix requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="better-git-of-theseus",
-    version="0.4.6",
+    version="0.5.0",
     description="Plot stats on Git repositories with interactive Plotly charts",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,7 +23,8 @@ setup(
         "plotly",
         "streamlit",
         "python-dateutil",
-        "scipy"
+        "scipy",
+        "matplotlib"
     ],
     entry_points={
         "console_scripts": [
