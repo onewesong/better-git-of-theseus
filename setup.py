9852b13123d7b49375acd96f6bb558786881b0cc
wesong <onewesong@gmail.com>
2026-02-03 17:05:55 +0800

    chore: bump version to 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="better-git-of-theseus",
-    version="0.5.0",
+    version="0.5.1",
     description="Plot stats on Git repositories with interactive Plotly charts",
     long_description=long_description,
     long_description_content_type="text/markdown",
